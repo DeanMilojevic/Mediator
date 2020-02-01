89f626b8cc1f2d822c1fe35d9d873c720a25be1c
Dean Milojevic <milojevicdean@gmail.com>
2020-02-01 15:21:25 +0100

    Adding null checks

--- src/Mediator.AspNetCore.DependencyInjection.Extensions/ServiceProviderDispatcher.cs
+++ src/Mediator.AspNetCore.DependencyInjection.Extensions/ServiceProviderDispatcher.cs
@@ -1,36 +1,55 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace Mediator.AspNetCore.DependencyInjection.Extensions
 {
     internal sealed class ServiceProviderDispatcher : IDispatcher
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly ILogger<ServiceProviderDispatcher> _logger;
 
-        public ServiceProviderDispatcher(IServiceProvider serviceProvider, ILogger<ServiceProviderDispatcher> logger)
+        public ServiceProviderDispatcher(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
-            _logger = logger;
         }
 
         public async Task Send(ICommand command)
         {
+            if (command is null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
             var genericType = typeof(ICommandHandler<>).MakeGenericType(command.GetType());
 
             var commandHandler = (dynamic)_serviceProvider.GetService(genericType);
+
+            EnsureHandlerExists(commandHandler, "The command handler is not registered.");
 
             await commandHandler.Handle(command);
         }
 
         public async Task<T> Send<T>(IQuery<T> query)
         {
+            if (query is null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
             var genericType = typeof(IQueryHandler<,>).MakeGenericType(query.GetType(), typeof(T));
 
             var queryHandler = (dynamic)_serviceProvider.GetService(genericType);
 
+            EnsureHandlerExists(queryHandler, "The query handler is not registered.");
+
             return (T) await queryHandler.Handle(query);
+        }
+
+        private void EnsureHandlerExists(dynamic handler, string errorMessage)
+        {
+            if (handler is null)
+            {
+                throw new ArgumentNullException(errorMessage);
+            }
         }
     }
 }
